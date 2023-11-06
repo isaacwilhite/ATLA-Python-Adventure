351595691a214e6d68484114ebd268b853b3a1a9
Tiana Lopez <tianadavin@gmail.com>
2023-11-06 18:37:01 -0500

    Adds get skill for player function

--- classes/abilities.py
+++ classes/abilities.py
@@ -7,6 +7,7 @@ class Abilities:
     def __init__(self, player_id, skill_id):
         self.player_id = player_id
         self.skill_id = skill_id
+        self.skills = []
 
     @property
     def player_id(self):
@@ -35,7 +36,8 @@ class Abilities:
                 id INTEGER PRIMARY KEY,
                 player_id INTEGER,
                 skill_id INTEGER
-                FOREIGN KEY (player_id, skill_id) REFERENCES player(id), skill(id)
+                FOREIGN KEY (player_id) REFERENCES player(id),
+                FOREIGN KEY (skill_id) REFERENCES skill(id)
             )
         """
         CURSOR.execute(sql)
@@ -58,7 +60,50 @@ class Abilities:
         CONN.commit()
 
     @classmethod
-    def create(cls, player_id, skill_id):
+    def find_by_player_skill(cls, player_id, skill_id):
+        ##return abilities instance having the player_id and skill_id
+        sql = """
+            SELECT * FROM abilities WHERE player_id = ? AND skill_id = ?
+        """
+        CURSOR.execute(sql, (player_id, skill_id))
+        record = CURSOR.fetchone()
+
+        if record:
+            return cls(*record)
+        else:
+            return None
+
+
+    ##add to the db
+    @classmethod
+    def create_db_instance(cls, player_id, skill_id):
+        #check if record exists
+        existing_ability = cls.find_by_player_skill(player_id, skill_id)
+
+        if existing_ability:
+            raise Exception("You already have this ability")
+
         ability = cls(player_id, skill_id)
         ability.save()
         return ability
+
+    ##get all the skills the player has by name
+    @classmethod
+    def get_skills_for_player(cls, player_id):
+        sql = """
+            SELECT skill_id FROM abilities WHERE player_id = ?
+        """
+        CURSOR.execute(sql, (player_id,))
+        skill_ids = CURSOR.fetchall()
+
+        skill_list = []
+        for skill_id in skill_ids:
+            sql = """
+                SELECT name from skill WHERE id = ?
+            """
+            CURSOR.execute(sql, (skill_id[0],))
+            skill_name = CURSOR.fetchone()
+            if skill_name:
+                skill_list.append(skill_name[0])
+
+        return skill_list
