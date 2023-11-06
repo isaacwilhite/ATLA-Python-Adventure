b5f1118115647ea9c0193a42d379e800b9d6a106
Tiana Lopez <tianadavin@gmail.com>
2023-11-05 22:15:21 -0500

    adds skill class

---
+++ classes/player.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+CONN = sqlite3.connect("database.db")
+CURSOR = CONN.cursor()
+
+class Player:
+
+    def __init__(self, username="", health = 10, id=None):
+        self.username = username.lower()
+        self.health = health
+        self.points = 0
+        self.id = id
+
+    @property
+    def username(self):
+        return self._username
+    @username.setter
+    def username(self, new_user):
+        if not isinstance(new_user, str):
+            raise TypeError("Username must be a string")
+        elif len(new_user) not in range(1, 11):
+            raise Exception("Username should be between 1 and 10 characters")
+        elif not hasattr(self, "username"):
+            raise Exception("Username cannot be reset")
+        else:
+            self._username = new_user
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS players (
+                id INTEGER PRIMARY KEY,
+                username TEXT,
+                health INTEGER
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
--- classes/skill.py
+++ classes/skill.py
@@ -4,10 +4,30 @@ CONN = sqlite3.connect("database.db")
 CURSOR = CONN.cursor()
 
 class Skill:
-  def __init__(self, name, description, point_cost):
-    self.name = name
-    self.description = description
-    self.point_cost = point_cost
+    def __init__(self, name, description, point_cost):
+        self.name = name
+        self.description = description
+        self.point_cost = point_cost
 
-  @property
-  def name(self):
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and not hasattr(self, 'name'):
+            self._name = new_name
+        else:
+            raise Exception("Name must be a string and cannot be reset")
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+        CREATE TABLE IF NOT EXISTS skills (
+            id INTEGER PRIMARY KEY
+            name TEXT,
+            description TEXT,
+            point_cost INTEGER
+        )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
