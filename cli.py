9e7a9cb915f597cacbbf2c78d34acecf1c81a87e
Tiana Lopez <tianadavin@gmail.com>
2023-11-06 23:45:09 -0500

    finishes login function

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import click
 import sys
+from classes.player import *
 
 from helpers import exit_program
 
@@ -17,31 +18,51 @@ def user_prompt():
     while True:
         user_choice = click.prompt("Your choice", type=click.Choice(['yes', 'no']))
         if user_choice == 'yes':
-            login_existing_user()
+            return login_existing_user()
         elif user_choice == 'no':
-            create_new_user()
+            return create_new_user()
         elif user_choice == 'quit': #checks to see if player wants to quit
             check_quit()
         else:
             click.echo("Invalid choice. Please enter 'yes' or 'no'.")
 
 def create_new_user():
-    # Implement the logic to create a new user
-    # You can prompt for additional user information if needed
-    click.echo("Welcome, new Avatar! You can create your character now.")
-    # Add code to handle user registration
+    click.echo("Welcome, new Avatar! You can create your username now.")
+    username = click.prompt("Please input your username:", type=str).lower().strip()
+    if username == 'quit':
+        check_quit(username)
+
+    existing_player = Player.find_by_username(username)
+    if existing_player:
+        click.echo("User already exists. Would you like to log in instead? (yes/no)")
+        while True:
+            choice = click.prompt("Log in instead?", type=click.Choice(['yes', 'no']))
+            if choice == 'yes':
+                return login_existing_user()
+            elif choice == 'no':
+                click.echo("Please choose a different username")
+            elif choice == "quit":
+                check_quit(choice)
+            else:
+                click.echo("Invalid choice. Please enter 'yes' or 'no'.")
+    else:
+        new_player = Player(username=username)
+        new_player.save()
+        return new_player
 
 def login_existing_user(username):
-    username = click.prompt("Please input your username:", type=str).lower().strip()
-    #check if they wanna quit
-    check_quit(username)
-
-
-
-    # Implement the logic to log in an existing user
-    # You can prompt for the user's password or other login details
-    click.echo(f"Welcome back, {username}! You can resume your journey as an Avatar.")
-    # Add code to handle user login
+    while True:
+        username = click.prompt("Please input your username:", type=str).lower().strip()
+        #check if they wanna quit
+        if username == 'quit':
+            check_quit(username)
+            return
+        existing_player = Player.find_by_username(username)
+        if existing_player:
+            click.echo(f"Welcome back, {username}! You can resume your journey as an Avatar.")
+            return existing_player
+        else:
+            click.echo("We cannot find your username. Please try again or type 'quit' to exit.")
 
 ##exit function
 def check_quit(string):
@@ -52,4 +73,3 @@ def check_quit(string):
 if __name__ == "__main__":
     welcome()
     user_prompt()
-    exit_program
